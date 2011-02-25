2627acaa31c773f91b0043ceaf362bd718394ad2
Paul Battley <pbattley@gmail.com>
2011-02-25 18:29:29 +0000

    Poll for updates.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var http      = require('http'),
     io        = require('socket.io'),
     sys       = require('sys'),
     express   = require('express'),
-    instagram = require('./lib/instagram');
+    instagram = require('./lib/instagram'),
+    person    = require('./lib/person'),
+    config    = require('./config/config.js');
 
-var config = require('./config/config.js');
+var api = new instagram.client(config.clientId, config.accessToken);
 
 var server = express.createServer();
 server.use(express.staticProvider(__dirname + '/public'));
@@ -12,6 +14,27 @@ server.use(express.errorHandler({showStack: true, dumpExceptions: true}));
 server.listen(config.port);
 
 var socket = io.listen(server);
+
+var i, people = [];
+for (i = 0; i < config.users.length; i++) {
+  people.push(new person.person(config.users[i]));
+}
+
+var updated = function(p){
+  sys.log('Update for ' + p.username);
+};
+
+var poll = function(){
+  setTimeout(poll, 60000);
+  var i;
+  for (i = 0; i < people.length; i++) {
+    people[i].getLatestUpdate(api, function(p){
+      if (p) {
+        updated(p);
+      }
+    });
+  }
+};
 
 socket.on('connection', function(client){
   var connected = true;
@@ -21,14 +44,6 @@ socket.on('connection', function(client){
   client.on('disconnect', function(){
     connected = false;
   });
-
-  var tick = function(){
-    if (!connected) {
-      return;
-    }
-    client.send((new Date()) + '');
-    setTimeout(tick, 1000);
-  };
-
-  tick();
 });
+
+poll();
