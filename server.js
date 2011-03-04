9416e1c4460be24b0a1c8d0e2b505bfe8d7bd73b
Paul Battley <pbattley@gmail.com>
2011-03-04 16:01:05 +0000

    Send updates.

--- server.js
+++ server.js
@@ -23,66 +23,86 @@ var handle = errorHandler.handler(function(err){
   printErr(err);
 });
 
-console.log(people);
+var main = function(dbCollection){
 
-database.withCollection(config, handle(function(db, collection){
-  async.forEach(config.userIds, function(userId, done){
-    collection.find({'user.id': userId}, {limit: 1, sort: {id: -1}}, handle(function(cursor){
-      cursor.toArray(handle(function(docs){
-        people[userId].setMinId(docs[0].id);
-        done();
-      }));
+  // Find latest update ID for each person
+  // on response:
+  //   - start polling for person.
+  //
+  Object.keys(people).forEach(function(userId){
+    var person = people[userId];
+    dbCollection.getLatestUpdateId(person.userId, handle(function(n){
+      person.setMinId(n);
+      var poll = function(){
+        console.log('polling '+person.userId);
+        person.getLatestUpdate(instagram, function(err, res){
+          if (err) {
+            pollInterval = config.pollInterval.error;
+            printErr(err);
+          } else {
+            pollInterval = config.pollInterval.normal;
+            if (res) { updateReceived(res); }
+          }
+        });
+        setTimeout(poll, pollInterval);
+      };
+      poll();
     }));
-  }, handle(function(){
-    console.log(people);
-  }));
-}));
-
-    /*
-server.get('/all.json', function(req, res){
-  res.header('Content-Type', 'application/json');
-  res.send(bulkData);
-});
-
-server.socket.on('connection', function(client){
-  server.clientPool.add(client);
-  client.on('disconnect', function(){
-    server.clientPool.remove(client);
   });
-  client.send(bulkData);
-});
 
-var regenerateBulkData = function(){
-  async.map(people, function(item, done){
-    done(null, item.lastUpdate);
-  }, handle(function(results){
-    bulkData = JSON.stringify(results);
-  }));
-};
+  // For each update received:
+  //   - save to database
+  //   - broadcast to all clients
+  //
+  var updateReceived = function(update){
+    console.log('received update for '+update.user.id);
+    dbCollection.insert(update, handle());
+    server.clientPool.broadcast(JSON.stringify(['new', [person.lastUpdate]]));
+  };
 
-var updated = function(person){
-  sys.log(['Update for', person.username].join(' '));
-  server.clientPool.broadcast(JSON.stringify([person.lastUpdate]));
-  regenerateBulkData();
-};
-
-var poll = function(){
-  setTimeout(poll, pollInterval);
-  sys.log('Polling');
-
-  async.forEach(people, function(person, done){
-    person.getLatestUpdate(instagram, function(err, res){
-      if (err) {
-        pollInterval = config.pollInterval.error;
-        printErr(err);
-      } else {
-        pollInterval = config.pollInterval.normal;
-        if (res) { updated(res); }
-      }
-      done();
+  // On request for new data:
+  // if timestamp given:
+  //   - send batch before timestamp
+  // else:
+  //   - send starting batch (latest)
+  //
+  var sendUpdates = function(client, timestamp){
+    var criteria, header;
+    if (timestamp) {
+      criteria = {created_time: {$lt: timestamp}};
+      header = 'more';
+    } else {
+      criteria = {};
+      header = 'start';
+    }
+    dbCollection.getUpdates(criteria, config.chunkSize, handle(function(data)){
+      client.send(JSON.stringify([header, data]));
     });
-  }, handle());
+  };
+
+  // On socket connection:
+  //   - Add to pool
+  //   - Set up disconnection handler
+  //   - Set up message handler
+  //   - Send an initial set of updates
+  //
+  server.socket.on('connection', function(client){
+    server.clientPool.add(client);
+    client.on('disconnect', function(){
+      server.clientPool.remove(client);
+    });
+    client.on('message', function(m){
+      var message = JSON.parse(m);
+      switch (message[0]) {
+        case 'more':
+          sendUpdates(client, message[1]);
+          break;
+      }
+    });
+    sendUpdates(client);
+  });
 };
 
-poll();
-*/
+database.withCollection(config, handle(function(collection){
+  main(collection);
+}));
