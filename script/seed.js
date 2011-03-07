1f19d1f0ed867391f4f27d1210daf8b298418263
Paul Battley <pbattley@gmail.com>
2011-03-07 11:43:34 +0000

    Update to seed script necessitated by lib changes.

--- script/seed.js
+++ script/seed.js
@@ -1,20 +1,20 @@
-var sys          = require('sys'),
-    async        = require('async'),
-    config       = require('../config/config').config,
-    instagram    = require('../lib/instagram').
+var sys       = require('sys'),
+    async     = require('async'),
+    config    = require('../config/config').config,
+    instagram = require('../lib/instagram').
                      createClient(config.clientId, config.accessToken),
-    errorHandler = require('../lib/errorhandler'),
-    database     = require('../lib/database');
+    util      = require('../lib/util'),
+    database  = require('../lib/database');
 
-var handle = errorHandler.handler(function(err){
+var E = util.errorHandler(function(err){
   console.log(err.stack);
 });
 
-database.withCollection(config, handle(function(collection){
+database.withCollection(config, E(function(collection){
   async.forEach(config.userIds, function(userId, done){
-    instagram.getUserMedia(userId, {count: 1000}, handle(function(resultSet){
+    instagram.getUserMedia(userId, {count: 1000}, E(function(resultSet){
       if (resultSet.hasOwnProperty('data')) {
-        collection.insert(resultSet.data, handle(function(){
+        collection.insert(resultSet.data, E(function(){
           console.log(resultSet.data.length + ' records added');
           done();
         }));
